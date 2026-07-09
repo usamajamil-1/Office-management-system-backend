32a14133938b33427be7580f760dce7259edcf09
Usama Jamil <usamajamil176@gmail.com>
2026-07-09 16:02:05 +0500

    Fix case-sensitive require paths for Linux deployment

--- routes/task.js
+++ routes/task.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const protect = require('../middleware/auth')
 
-const {createTask , readTask ,  deleteTask, updateTask} = require('../controllers/TaskController')
+const {createTask , readTask ,  deleteTask, updateTask} = require('../controllers/taskController')
 router.post('/',protect,createTask)
 router.get('/', protect, readTask)
 router.delete('/:id', protect, deleteTask)
