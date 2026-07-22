0c93a7f1d35c4a72943181b9bb8f4987a86afbd6
Usama Jamil <usamajamil176@gmail.com>
2026-07-22 20:42:46 +0500

    fix: attendance dto se date required hata diya

--- modules/attendance/attendance.dto.js
+++ modules/attendance/attendance.dto.js
@@ -2,7 +2,6 @@ const Joi = require('joi')
 
 const attendanceSchema = Joi.object({
   employee: Joi.string().hex().length(24).required(),
-  date: Joi.date().required(),
   status: Joi.string().valid('Present', 'Absent'),
 })
 
